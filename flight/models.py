1b64021a0a6638746a7bc24ce64bb9f669780fe9
Zai <zaiyellyintaung@gmail.com>
2023-05-08 04:55:47 +0630

    Done first migration

--- flight/models.py
+++ flight/models.py
@@ -3,5 +3,16 @@ from django.db import models
 # Create your models here.
 
 
+class Cities(models.Model):
+	name=models.CharField(max_length=69)
+
+
+class Airport(models.Model):
+	title=models.CharField(max_length=69)
+	location=models.ForeignKey(Cities,on_delete=models.CASCADE,related_name="airport")
+
+
 class Flight(models.Model):
-	origin=models.char
+	departure=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="departing_flights")
+	destination=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="arriving_flights")
+	price=models.IntegerField()
