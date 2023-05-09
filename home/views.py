4f2c2f0a91b74cacbf312665dba4be3e7eb746e4
Zai <zaiyellyintaung@gmail.com>
2023-05-09 19:04:38 +0630

    Git hub upload

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import *
+
 
 def index(request):
 	return (render(request,"home/index.html"))
@@ -8,11 +10,23 @@ def about(request):
 	
 
 def flights(request):
-	return (render(request,"home/flights.html"))
+	all_flights=Flight.objects.all()
+
+	data={
+		"flights":all_flights,
+	}
+	return (render(request,"home/flights.html",data))
 
 
 def flightdetails(request,pk):
-	return (render(request,"home/flightdetails.html"))
+	flight=Flight.objects.get(id=pk)
+	city=City.objects.get()
+
+	data={
+		"flight":flight,
+		"city":city
+	}
+	return (render(request,"home/flightdetails.html",data))
 	
 
 def citydetails(request,pk):
