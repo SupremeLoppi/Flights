26c6ed2810ed2a2791f77e8b1edc6e0bd9d337f0
Zai <zaiyellyintaung@gmail.com>
2023-05-09 11:24:57 +0630

    Done db

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Places)
+admin.site.register(FlightCompany)
+admin.site.register(Airport)
+admin.site.register(Flight)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
 # Create your models here.
+
+class Country(models.Model):
+	name=models.CharField(max_length=50)
+
+	def __str__(self):
+		return f"{self.name}"
+
+class City(models.Model):
+	name=models.CharField(max_length=50)
+	Country=models.ForeignKey(Country,on_delete=models.CASCADE,related_name="countryx")
+
+	def __str__(self):
+		return f"{self.name}"
+
+
+
+class Places(models.Model):
+	popularity_choices=(
+		("high" , "high"),
+		("low", "low"),
+		("most popular","most popular"),
+		("least popular","least popular"),)
+
+	name=models.CharField(max_length=69)
+	popularity=models.CharField(max_length=69,choices=popularity_choices)
+	location=models.ForeignKey(City,on_delete=models.CASCADE,related_name="city_namep")
+
+	def __str__(self):
+		return f"{self.name}"
+
+
+class FlightCompany(models.Model):
+	name=models.CharField(max_length=50)
+	descriptions=models.TextField(default='')
+
+	def __str__(self):
+		return f"{self.name}"
+
+
+class Airport(models.Model):
+	name=models.CharField(max_length=50)
+	location=models.ForeignKey(City,on_delete=models.CASCADE,related_name="city_name")
+
+	def __str__(self):
+		return f"{self.name}"
+
+class Flight(models.Model):
+	class_choices = (
+        ('high class', 'high class'),
+        ('middle class', 'middle class'),
+        ('business class', 'business class'),
+    )
+
+	pricing_choices = (
+        (500, '500'),
+        (1000, '1000'),
+        (800, '800'),
+        (300, '300'),
+        (200, '200'),
+
+    )
+
+	code=models.CharField(max_length=6)
+	owner=models.ForeignKey(FlightCompany,on_delete=models.CASCADE,related_name="flight_company")
+	departure=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="departurex")
+	destination=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="destinationx")
+	pricing=models.IntegerField(choices=pricing_choices)
+	duration=models.IntegerField(default=300)
+	capacity=models.IntegerField(default=500)
+	standard=models.CharField(max_length=50,choices=class_choices)
+	
+	def __str__(self):
+		return f"{self.code}"
+
+
+
+
+
