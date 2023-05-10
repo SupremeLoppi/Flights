5e96c312c2b025b40e788dadfd0cd0739f9b8099
Zai <zaiyellyintaung@gmail.com>
2023-05-10 10:27:50 +0630

    Finally finished

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,8 @@ urlpatterns=[
 	path("about/",views.about,name="about"),
 	path("flights/",views.flights,name="flightlists"),
 	path("flights/<int:pk>",views.flightdetails,name="flightdetails"),
+	path("city",views.city,name="city"),
 	path("city/<int:pk>",views.citydetails,name="citydetails"),
+	path("flightcompany/<int:pk>",views.flightcompanydetails,name="flightcompanies"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -20,14 +20,58 @@ def flights(request):
 
 def flightdetails(request,pk):
 	flight=Flight.objects.get(id=pk)
-	city=City.objects.get()
+	flightcompany=FlightCompany.objects.get(name=flight.owner)
+
+	destination_airport=Airport.objects.get(name=flight.destination)
+	departure_airport=Airport.objects.get(name=flight.departure)
+
+	destination_city=City.objects.get(name=destination_airport.location)
+	departure_city=City.objects.get(name=departure_airport.location)
+
+	departure_city_id=departure_city.id
+	destination_city_id=destination_city.id
+
+
 
 	data={
 		"flight":flight,
-		"city":city
+		"flightcompany":flightcompany,
+		"destination_city":destination_city,
+		"departure_city":departure_city,
+		"destination_city_id":destination_city_id,
+		"departure_city_id" :departure_city_id
 	}
 	return (render(request,"home/flightdetails.html",data))
 	
+def city(request):
+
+	cities=City.objects.all()
+
+	data={
+		"cities":cities
+	}
+
+	return render(request,"home/cities.html",data)
+
 
 def citydetails(request,pk):
-	return (render(request,"home/citydetails.html"))
+
+	city=City.objects.get(id=pk)
+	places=Places.objects.filter(location=city)
+
+
+
+
+	data={
+		"city":city,
+		"places":places
+	}
+
+	return (render(request,"home/citydetails.html",data))
+
+def flightcompanydetails(request,pk):
+	flightcompany=FlightCompany.objects.get(id=pk)
+	data={
+		"flightcompany":flightcompany
+	}
+	return render(request,"home/flightcompanydetails.html",data)
